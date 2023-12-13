38f98a914e2ba5678a6fcff3410f6f10440a7a78
Patrick Garcia <trickrenzgarcia@gmail.com>
2023-12-14 05:09:42 +0800

    fix err build

--- components/client/Contract.tsx
+++ components/client/Contract.tsx
@@ -1,14 +1,8 @@
 "use client"
 
 import React, { useEffect } from 'react'
-import { useContract } from '@thirdweb-dev/react';
-export default function Contract() {
-  const { contract, isLoading, isError } = useContract("0x69b05D8ed116Bb160B8a268a4315D2767123eFA1");
-  
-  useEffect(() => { 
-    console.log(contract)
-  }, [])
 
+export default function Contract() {
   return (
     <div>
 
